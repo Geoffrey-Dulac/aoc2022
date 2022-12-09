296ca9a90773d13d55cf338ad663df55385a251a
Geoffrey Dulac <geoffrey.dulac@nventive.com>
2022-12-08 21:41:29 -0500

    ok

--- 06_part2.rb
+++ 06_part2.rb
@@ -2,13 +2,14 @@ file = File.open("./06_part2.txt")
 file_data = file.readlines.map(&:chomp)
 
 chars = file_data[0].split('')
+pocket_size = 14
 
 result = 0
 chars.each_with_index() do |char, i|
-    pocket = chars[i..i+13].uniq
+    pocket = chars[i..i+(pocket_size - 1)].uniq
 
-    if pocket.length == 14
-        result = i + 14
+    if pocket.length == pocket_size
+        result = i + pocket_size
         break
     end
 end
