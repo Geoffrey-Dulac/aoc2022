ff2f799e05c21d698ef73f2818a917d1935138c0
Geoffrey Dulac <geoffrey.dulac@nventive.com>
2022-12-08 21:42:30 -0500

    ok

--- 06_part1.rb
+++ 06_part1.rb
@@ -2,13 +2,14 @@ file = File.open("./06_part1.txt")
 file_data = file.readlines.map(&:chomp)
 
 chars = file_data[0].split('')
+pocket_size = 4
 
 result = 0
 chars.each_with_index() do |char, i|
-    pocket = chars[i..i+3].uniq
+    pocket = chars[i..i+(pocket_size - 1)].uniq
 
-    if pocket.length == 4
-        result = i + 4
+    if pocket.length == pocket_size
+        result = i + pocket_size
         break
     end
 end
